16eafffcff747de630d9dee5f730e9f6aa8f87f2
mikedaabeast <michaelarboleda1231@hotmail.com>
2016-04-17 05:26:14 -0400

    Tested Nav Bar and Resources and Assisment

--- Test_Protractor/e2e.js
+++ Test_Protractor/e2e.js
@@ -1,17 +1,49 @@
 describe('Core Tests:', function() {
 
-  beforeEach(function() {
-    browser.get('http://localhost:3000/');
-  });
+	beforeEach(function() {
+		browser.get('http://localhost:3000/');
+	});
 
-  afterEach(function() {
-    browser.sleep(3000);
-  });
-  
-  it('should send user to contact page', function() {
-    element(by.id('contact')).click();
+	afterEach(function() {
+		browser.sleep(3000);
+	});
 
-    expect(browser.getCurrentUrl()).toBe('http://localhost:3000/contact');
-  });
-  
-});  
+	describe('Nav Bar Tests', function(){
+		//Test Contact
+		it('should send user to contact page', function() {
+			element(by.id('contact')).click();
+
+			expect(browser.getCurrentUrl()).toBe('http://localhost:3000/contact');
+		});
+		
+		//Test About 
+		it('should send user to contact page', function() {
+			element(by.id('about')).click();
+
+			expect(browser.getCurrentUrl()).toBe('http://localhost:3000/about');
+		});
+		
+		//Test Login
+		it('should send user to contact page', function() {
+			element(by.id('login')).click();
+
+			expect(browser.getCurrentUrl()).toBe('http://localhost:3000/authentication/signin');
+		});
+		
+	});  
+	
+	// test resources page route
+	it('should send user to subject resources page', function() {
+		//Testing Biochemistry Page
+		element(by.repeater('subject in subjects').row(11)).element(by.id('resources')).click();
+		expect(browser.getCurrentUrl()).toBe('http://localhost:3000/Chemistry%20&%20Biochemistry/resources');
+	});
+
+	// test assessment page route
+	it('should send user to subject resources page', function() {
+		//Testing 
+		element(by.repeater('subject in subjects').row(1)).element(by.id('assessment')).click();
+		expect(browser.getCurrentUrl()).toBe('http://localhost:3000/Genetics/quiz');	
+	});
+
+}); 
