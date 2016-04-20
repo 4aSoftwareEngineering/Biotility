a2e32622a28de8cdef93b79db93d74d2cee04f2f
Isabel Laurenceau <isalau@ufl.edu>
2016-04-19 21:29:35 -0400

    add grunt register

--- gruntfile.js
+++ gruntfile.js
@@ -9,6 +9,7 @@ var _ = require('lodash'),
   fs = require('fs'),
   path = require('path');
 
+
 module.exports = function (grunt) {
   // Project Configuration
   grunt.initConfig({
@@ -289,3 +290,4 @@ module.exports = function (grunt) {
   // Run the project in production mode
   grunt.registerTask('prod', ['build', 'env:prod', 'mkdir:upload', 'copy:localConfig', 'concurrent:default']);
 };
+grunt.registerTask('heroku:production', 'clean less mincss uglify');
